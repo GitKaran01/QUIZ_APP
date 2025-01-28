3fa13c02312b31e686179185c85353e2c58051bb
GitKaran01 <karansinha866@gmail.com>
2025-01-29 03:44:01 +0530

    applied center widget to column

--- lib/startscreen.dart
+++ lib/startscreen.dart
@@ -7,38 +7,40 @@ class Startscreen extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        SizedBox(
-          height: 50,
-        ),
-        Image.asset(
-          "assets/images/quiz-logo.png",
-          scale: 2,
-          color: const Color.fromARGB(204, 255, 255, 255),
-        ),
-        SizedBox(
-          height: 25,
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        Text(
-          "Learn flutter,",
-          style: TextStyle(fontSize: 20, color: Colors.white),
-        ),
-        SizedBox(
-          height: 50,
-        ),
-        ElevatedButton(
-          style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
-          onPressed: () {
-            fun();
-          },
-          child: Text("Start Quiz"),
-        ),
-      ],
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          Image.asset(
+            "assets/images/quiz-logo.png",
+            scale: 2,
+            color: const Color.fromARGB(204, 255, 255, 255),
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            "Learn flutter",
+            style: TextStyle(fontSize: 20, color: Colors.white),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
+            onPressed: () {
+              fun();
+            },
+            child: Text("Start Quiz"),
+          ),
+        ],
+      ),
     );
   }
 }
