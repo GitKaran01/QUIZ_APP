9811a2a598ffc4d200224171b2781dd6eee9e474
GitKaran01 <karansinha866@gmail.com>
2025-01-29 03:30:32 +0530

    FIRST COMMIT

---
+++ lib/quiz.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/questions.dart';
+import 'package:quiz_app/startscreen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+  @override
+  State<Quiz> createState() {
+    return _Quiz();
+  }
+}
+
+class _Quiz extends State<Quiz> {
+
+  var currentScreen = 'startscreen';
+
+  void switchpage() {
+    setState(() {
+      currentScreen = 'questionscreen';
+    });
+  }
+
+  @override
+  Widget build(context) {
+
+    var screenWidget =  currentScreen == 'startscreen' ? Startscreen(switchpage) : Questions();
+
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            colors: [Colors.deepPurple, Colors.blue],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight),
+      ),
+      child: screenWidget,
+    );
+  }
+}
---
+++ lib/startscreen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class Startscreen extends StatelessWidget {
+  const Startscreen(this.fun, {super.key});
+
+  final void Function() fun;
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SizedBox(
+          height: 50,
+        ),
+        Image.asset(
+          "assets/images/quiz-logo.png",
+          scale: 2,
+          color: const Color.fromARGB(204, 255, 255, 255),
+        ),
+        SizedBox(
+          height: 25,
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          "Learn flutter,",
+          style: TextStyle(fontSize: 20, color: Colors.white),
+        ),
+        SizedBox(
+          height: 50,
+        ),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
+          onPressed: () {
+            fun();
+          },
+          child: Text("Start Quiz"),
+        ),
+      ],
+    );
+  }
+}
