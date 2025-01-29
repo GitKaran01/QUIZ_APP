b210eaa824701e043759f9fe52598782964a036d
GitKaran01 <karansinha866@gmail.com>
2025-01-29 13:42:14 +0530

    we have shufffled the list but the function is void so we will modify the code that we get the shuffled list also.

--- lib/models/quiz_questions.dart
+++ lib/models/quiz_questions.dart
@@ -1,7 +1,13 @@
 class QuizQuestion {
-  const QuizQuestion(this.text,this.answers);
+  const QuizQuestion(this.text, this.answers);
 
   final String text;
   final List<String> answers;
-}
 
+  List<String> getShuffledAnswers() {
+    final List.of(answers).shuffle();
+     // here we copied the list and shuffled it.
+     // we are calling a method on the result of function of another method.
+
+  }
+}
