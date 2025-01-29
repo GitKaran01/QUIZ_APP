91b7dd702c20edf3c707a1e90b99727add092181
GitKaran01 <karansinha866@gmail.com>
2025-01-29 06:33:51 +0530

    showing question and answers but just for 1 question

--- lib/questions._screen.dart
+++ lib/questions._screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,26 +13,35 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreen extends State<QuestionsScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
+
+    
+
     return SizedBox(
       width: double.infinity,
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           Text(
-            "The quesiton...",
-            style: TextStyle(color: Colors.white),
+            currentQuestion.text,
+            style: const TextStyle(color: Colors.white),
           ),
           SizedBox(height: 30),
           AnswerButton(
-            answerText: "Answer text...",
+            answerText: currentQuestion.answers[0],
             onTap: () {},
           ),
           AnswerButton(
-            answerText: "Answer text...",
+            answerText:currentQuestion.answers[1],
             onTap: () {},
           ),
           AnswerButton(
-            answerText: "Answer text...",
+            answerText: currentQuestion.answers[2],
+            onTap: () {},
+          ),
+
+            AnswerButton(
+            answerText: currentQuestion.answers[3],
             onTap: () {},
           ),
         ],
