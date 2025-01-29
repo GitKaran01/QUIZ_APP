5a4f1f11712cc90e9f608e5d95496dda27e680fc
GitKaran01 <karansinha866@gmail.com>
2025-01-29 11:05:06 +0530

    sworking

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 
 class AnswerButton extends StatelessWidget {
-  const AnswerButton({super.key, required this.answerText, required this.onTap});
+  const AnswerButton(
+      {super.key, required this.answerText, required this.onTap});
 
   final String answerText;
   final void Function() onTap;
 
   @override
   Widget build(context) {
+    
     return ElevatedButton(
       onPressed: onTap,
-      style: ElevatedButton.styleFrom(),
+      style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15))),
       child: Text(answerText),
     );
   }
