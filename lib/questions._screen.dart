d435a59424f51a46ffe86f2cecb536f9197832d0
GitKaran01 <karansinha866@gmail.com>
2025-01-31 00:23:13 +0530

    incremented question index and added google fonts to the questions

--- lib/questions._screen.dart
+++ lib/questions._screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
 import 'package:quiz_app/data/questions.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -11,35 +12,52 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreen extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  answerQuestion() {
+    // currentQuestionIndex = currentQuestionIndex + 1;
+    // currentQuestionIndex += 1;
+    setState(() {
+      currentQuestionIndex++; // increment by 1;
+    });
+  }
+
   @override
   Widget build(context) {
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[currentQuestionIndex];
 
-    return SizedBox(
-      width: double.infinity,
-      child: Container(
-        margin: EdgeInsets.all(5),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              currentQuestion.text,
-              style: const TextStyle(color: Colors.white, fontSize: 30),
-              textAlign: TextAlign.center,
-            ),
+    return Center(
+      child: SizedBox(
+        width: double.infinity,
+        height: 400,
+        child: Container(
+          margin: EdgeInsets.only(left: 90, right: 90),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                currentQuestion.text,
+                style: GoogleFonts.oswald(color: Colors.white, fontSize: 50),
+                textAlign: TextAlign.center,
+              ),
 
-            SizedBox(height: 30),
+              SizedBox(height: 30),
 
-            ...currentQuestion.answers.map((answer) {
-              return AnswerButton(
-                answerText: answer,
-                onTap: () {},
-              );
-            })
-            // Now answer buttons will get created dynamically according to the number of questions given.
-            // Mapping list using spread operator
-          ],
+              ...currentQuestion.getShuffledAnswers().map((answer) {
+                // ...currentQuestion.answers.map((answer) {
+
+                // We will apply shuffle function here, it changes the original data.
+                // while ... spread function doesn't
+                return AnswerButton(
+                  answerText: answer,
+                  onTap: answerQuestion,
+                );
+              })
+              // Now answer buttons will get created dynamically according to the number of questions given.
+              // Mapping list using spread operator
+            ],
+          ),
         ),
       ),
     );
